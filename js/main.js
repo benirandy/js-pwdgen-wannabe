0b18827c85e353f09833a75b0dc719dd72c4c021
Beni Randy Harold <haroldbeni111997@gmail.com>
2023-02-21 17:00:27 +0100

    Dando una struttura all'esercizio

---
+++ js/main.js
@@ -0,0 +1,20 @@
+//Devo creare un Password generator
+
+
+/*
+
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito,
+E infine devo fare aparire sia il nome,il cognome e il suo colore preferito sulla pagina.
+
+
+*/
+
+let nomeDellapersona = prompt('Inserisci il tuo nome');
+
+
+const cognomeDellapersona = prompt('inserisci il tuo cognome');
+
+
+let coloreDellapersona = prompt('inserisci il tuo colore preferito');
